6a9642dfe7631a5be6002075005fa3234406bcc1
enaanai <aenaanai@uae.ac.ma>
2025-06-23 17:15:51 +0100

    Commit2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end("👋 Bonjour depuis Node.js + GitHub Actions !");
+  res.end("J'ai modifié le message");
 });
 server.listen(3000, () => console.log("Serveur en écoute sur le port 3000")
 );
